45c05e3f12f17e07ba887bc5797cbb1c5bc1df3e
Mickey Lin <mickeykiller113@gmail.com>
2022-09-02 14:08:40 +0800

    test git commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['GET', 'POST']) 
+def login():
+    if request.method == 'POST': 
+        return 'Hello ' + request.values['username'] 
+
+    return "<form method='post' action='/login'><input type='text' name='username' />" \
+            "</br>" \
+           "<button type='submit'>Submit</button></form>"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run() 
