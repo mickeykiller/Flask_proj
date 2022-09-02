1157a90dd466376ebc1adda46894690f1ada797d
Mickey Lin <mickeykiller113@gmail.com>
2022-09-02 15:09:51 +0800

    test branch

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, request
-from flask import render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST']) 
+@app.route('/loginurl', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST': 
-        return 'Hello ' + request.values['username'] 
-
-    return "<form method='post' action='/login'><input type='text' name='username' />" \
-            "</br>" \
-           "<button type='submit'>Submit</button></form>"
+    #  利用request取得使用者端傳來的方法為何
+    if request.method == 'POST':
+                          #  利用request取得表單欄位值
+        return 'Hello ' + request.values['username']
+    
+    #  非POST的時候就會回傳一個空白的模板
+    return render_template('login.html')
 
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     app.run() 
